3e04e4fe3666ced0791e7d25f77de26eca7afb1e
sameh sayed <samehsayed628@gmail.com>
2023-07-21 18:40:30 +0300

    upload

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const MongoDBStore = require("connect-mongodb-session")(sessions);
 const flash = require("express-flash");
 require("express-async-errors");
 const apiError = require("./helps/apiError");
-const http = require("http");
-const reload = require("reload");
+// const http = require("http");
+// const reload = require("reload");
 
 // create app
 const app = express();
@@ -68,10 +68,9 @@ const contactusRoutes = require("./routes/contacts.routes");
 app.use("/contactus", contactusRoutes);
 
 const homeRoute = require("./routes/home.routes");
-const { default: mongoose } = require("mongoose");
 app.use("/", homeRoute);
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 const port = process.env.PORT;
-server.listen(port, () => console.log(`server working on port ${port}`));
-reload(app);
+app.listen(port, () => console.log(`server working on port ${port}`));
+// reload(app);
